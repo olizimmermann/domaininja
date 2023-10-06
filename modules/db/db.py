fe7b6f3d2565684796eb7ece402af58a52fad333
olizimmermann <github@ozimmermann.com>
2023-10-06 23:04:22 +0200

    with connection.cursor

--- modules/db/db.py
+++ modules/db/db.py
@@ -6,7 +6,6 @@ class Database:
     
     def __init__(self, host: str, port: int, username: str, password: str, database: str):
         self.connection = None
-        self.cursor = None
         self.connect(host, port, username, password, database)
         if self.connection is not None:
             print("Connected to database")
@@ -16,11 +15,21 @@ class Database:
         """ Connects to MySQL database """
         try:
             self.connection = pymysql.connect(host=host, port=port, user=username, passwd=password, db=database, charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
-            self.cursor = self.connection.cursor()
         except Exception as e:
             print("Error connecting to database: " + str(e))
             return False
         return True
+    
+    def is_connected(self):
+        """ Checks if the database is connected """
+        if self.connection is None:
+            return False
+        return True
+    
+    def check_connection(self):
+        """ Checks the database connection and reconnects if necessary """
+        if not self.is_connected():
+            self.connect()
     
     def init_database(self):
         """ Creates the tables if they do not exist """
@@ -33,34 +42,41 @@ class Database:
                 for sql in sql_file.split(";"):
                     if sql == "" or sql == "\n" or sql.strip() == "":
                         continue
-                    self.cursor.execute(sql)
-                    self.connection.commit()
+                    with self.connection.cursor() as cursor:
+                        cursor.execute(sql)
+                        self.connection.commit()
                 print("Created tables")
         else:
             print("init_db.sql does not exist")
             
     def get_domain(self, domain):
         """ Gets the domain from the database """
+        self.check_connection()
         sql = "SELECT * FROM domains WHERE domain = %s"
-        self.cursor.execute(sql, (domain,))
-        return self.cursor.fetchone()
+        with self.connection.cursor() as cursor:
+            cursor.execute(sql, (domain,))
+            return cursor.fetchone()
     
     def update_domain(self, domain):
         """ Updates the domain in the database """
+        self.check_connection()
         domain_entry = self.get_domain(domain)
         if domain_entry is None:
             self.insert_domain(domain)
             return
         times_seen = domain_entry["times_seen"] + 1
         sql = "UPDATE domains SET times_seen = %s WHERE domain = %s"
-        self.cursor.execute(sql, (times_seen, domain))
-        self.connection.commit()
+        with self.connection.cursor() as cursor:
+            cursor.execute(sql, (times_seen, domain))
+            self.connection.commit()
 
     def insert_domain(self, domain):
         """ Inserts the domain into the database """
+        self.check_connection()
         sql = "INSERT INTO domains (domain, times_seen) VALUES (%s, %s)"
-        self.cursor.execute(sql, (domain, 1))
-        self.connection.commit()
+        with self.connection.cursor() as cursor:
+            cursor.execute(sql, (domain, 1))
+            self.connection.commit()
         
         
         
